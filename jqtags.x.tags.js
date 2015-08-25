5d195e029d3d5ca71a5487604acaa0cebff34951
animesh <animesh@unicommerce.com>
2015-08-25 16:14:14 +0530

    initial value empty was causing bug: fixed

--- jqtags.x.tags.js
+++ jqtags.x.tags.js
@@ -1,12 +1,12 @@
 _tag_("jqtags.x.tags",function(select){
-	
-	var jq = module("jQuery");
-	
-	//$.fn.editable.defaults.mode = 'inline';
-	$.fn.editable.defaults.validate = function(v){
-		//console.log("validating",v);
-		//return "Fuck you"
-	};
+
+  var jq = module("jQuery");
+
+  //$.fn.editable.defaults.mode = 'inline';
+  $.fn.editable.defaults.validate = function(v){
+    //console.log("validating",v);
+    //return "Fuck you"
+  };
 
 
   var getCustomData = function(callback,queryCallback){
@@ -27,103 +27,107 @@ _tag_("jqtags.x.tags",function(select){
 
 
 
-	return {
-	    tagName: "jqx-tags",
-	    events: {
-	    },
-	    accessors: {
-	        value: {
-	            type: "string",
-	            default : "",
-	            onChange : "valueOnChange"
-	        },
-	        popup : {
-	        	type : "boolean",
-	        	default : true
-	        },
-          placeholder : {
-            type : "string",
-            default : "Select"
-          },
-          emptyString : {
-            type : "string",
-            default : "Select"
-          }
-	    },
-	    attachedCallback : function () {
-	    	var self = this;
-	    	this.$a = 
-	    	this.$.innerHTML = '<a href=# data-type=select2 data-title="'+ this.$.placeholder +'" >'+this.$.emptyString+'</a>';
-	    	this.$a =jQuery(this.$).find("a");
-          self.mySelectedOptions ={};
-          self.selected = [];
-          self.setValue(self.$.value);
+  return {
+    tagName: "jqx-tags",
+    events: {
+    },
+    accessors: {
+      value: {
+        type: "string",
+        default : "",
+        onChange : "valueOnChange"
+      },
+      popup : {
+        type : "boolean",
+        default : true
+      },
+      placeholder : {
+        type : "string",
+        default : "Enter Value"
+      },
+      emptyString : {
+        type : "string",
+        default : "Select"
+      }
+    },
+    attachedCallback : function () {
+      var self = this;
+      this.$a =
+        this.$.innerHTML = '<a href=# data-type=select2 data-title="'+ this.$.placeholder +'" ></a>';
+      this.$a =jQuery(this.$).find("a");
+      self.mySelectedOptions ={};
+      self.selected = [];
+      self.setValue(self.$.value);
 
-          self.trigger("jq.init",{
-            value : self.$.value,
-            populate : function(selected){
-              self.selected = selected;
-            }
-          });
+      self.trigger("jq.init",{
+        value : self.$.value,
+        populate : function(selected){
+          self.selected = selected;
+        }
+      });
 
-          self.$a.editable({
-            send : 'never',
-            select2: {
-              multiple: true,
-              data : [{id: 'us', text: 'United States'}],
-              tags : [],
-              placeholder : self.$.placeholder,
-              query : function(e,b) {
-                self.trigger("jq.query",{
-                  value : self.$.value,
-                  setOptions : function(options){
-                    return e.callback({
-                      results : options
-                    });
-                  }
+      self.$a.editable({
+        send : 'never',
+        select2: {
+          multiple: true,
+          data : [],
+          tags : [],
+          placeholder : self.$.placeholder,
+          query : function(e,b) {
+            self.trigger("jq.query",{
+              value : self.$.value,
+              setOptions : function(options){
+                return e.callback({
+                  results : options
                 });
-              },
-              formatSelection: function(item) {
-                self.mySelectedOptions[item.id] = item.text;
-                self.trigger("jq.selected",{
-                  item : item,
-                  format : function(disp){
-                    self.mySelectedOptions[item.id] = disp;
-                  }
-                });
-                return item.text;
-              },
-              initSelection: function (element, callback) {
-                callback(self.setValue(self.$.value))
               }
-            },
-            mode : (self.$.popup ? 'popup' : 'inline')
-           // title: 'Enter username'
-          }).on("save",debounce(function(e,params){
-            self.$.value = params.newValue;
-            self.setValue(params.newValue);
-            self.trigger("change");
-            self.trigger("input");
-          })).trigger("change");
-	    },
-	    detachedCallback : function(){
-        this.$a.editable("destroy");
-	    },
-      setValue : function(newValue){
-        if(newValue === undefined) return;
-        var finalValues = [];
-        var newValues = (newValue+"").split(",");
-        this.selected = [];
-        for(var i in newValues){
+            });
+          },
+          formatSelection: function(item) {
+            self.mySelectedOptions[item.id] = item.text;
+            self.trigger("jq.selected",{
+              item : item,
+              format : function(disp){
+                self.mySelectedOptions[item.id] = disp;
+              }
+            });
+            return item.text;
+          },
+          initSelection: function (element, callback) {
+            callback(self.setValue(self.$.value))
+          }
+        },
+        mode : (self.$.popup ? 'popup' : 'inline')
+        // title: 'Enter username'
+      }).on("save",debounce(function(e,params){
+        self.$.value = params.newValue;
+        self.setValue(params.newValue);
+        self.trigger("change");
+        self.trigger("input");
+      })).trigger("change");
+    },
+    detachedCallback : function(){
+      this.$a.editable("destroy");
+    },
+    setValue : function(newValue){
+      if(newValue === undefined) return;
+      var finalValues = [];
+      var newValues = (newValue+"").split(",");
+      this.selected = [];
+      for(var i in newValues){
+        if(!is.Empty(newValues[i])){
           finalValues.push(this.mySelectedOptions[newValues[i]] || newValues[i]);
           this.selected.push({ id : newValues[i], text : this.mySelectedOptions[newValues[i]] || newValues[i]});
         }
-        var finalValue = finalValues.join(",");
-        this.$a.text(!is.Empty(finalValue) ? finalValue : this.$.emptyString);
-        return this.selected;
-      },
-	    valueOnChange : function(e,oldValue,newValue){
-        this.setValue(newValue);
-	    }
-	};
+      }
+      var finalValue = finalValues.join(",");
+      if(!is.Empty(finalValue)){
+        this.$a.text(finalValue);
+      }
+      return this.selected;
+    },
+    valueOnChange : function(e,oldValue,newValue){
+      this.setValue(newValue);
+    }
+  };
 });
